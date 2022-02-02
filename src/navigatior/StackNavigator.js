ad876be3b94719148aa6bdaa08d27f3efc18e246
daniel2024 <daniehuancapiza@frba.utn.edu.ar>
2022-02-02 00:01:17 -0300

    fix in screen

--- src/navigatior/StackNavigator.js
+++ src/navigatior/StackNavigator.js
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{headerShown:false}}>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="IconsView" component={IconsView} />
             <Stack.Screen name="Back" component={Back} />
